f048efdca7a0f06784e7ac7e4d3bec6545a7d3f9
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-03-31 16:40:08 -0700

    March 31, 2022 Coding Challenge

--- main.js
+++ main.js
@@ -82,3 +82,31 @@ function queueTime(customers, n) {
 function arrayDiff(a, b) {
   return a.filter(x => !b.includes(x));
 }
+
+//Street Fighter selector using arrays
+function streetFighterSelection(fighters, position, moves){
+  let currentFighter = [];
+  for (let i = 0; i < moves.length; i++){
+    switch(moves[i]){
+        case ("up"):
+          position[0] = 0;
+          break;
+        case ("down"):
+          position[0] = 1;
+          break;
+        case ("left"):
+          position[1] == 0 ? position[1] = 5 : position[1] -= 1;
+          break;
+        case ("right"):
+          position[1] == 5 ? position[1] = 0 : position[1] += 1;
+          break;
+    }
+    currentFighter.push(fighters[position[0]][position[1]]);
+  }
+  return currentFighter;
+}
+
+//Find duplicates in a string
+function duplicateEncode(word){
+  return word.toLowerCase().split("").map((letter, index, self) => self.indexOf(letter) == self.lastIndexOf(letter) ? "(" : ")").join("");
+}
