b464e165282bd77cb2f3858538557548bb1bbba9
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-04-05 18:15:15 -0700

    April 5, 2022 Coding Challenge

--- main.js
+++ main.js
@@ -235,3 +235,15 @@ function listSquared(m, n) {
     return divisors;
   }
 }
+
+//Find if product of two Fib numbers equals given number
+function productFib(prod){
+  let first = 0;
+  let second = 1;
+  while (first * second < prod){
+    second += first;
+    first = second - first;
+  }
+  return [Math.min(first, second), Math.max(first, second), first * second == prod];
+}
+
