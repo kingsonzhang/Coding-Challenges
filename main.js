35204699b823ceb3afc05643e5454f76714fa8ff
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-04-19 15:30:51 -0700

    April 19, 2022 Coding Challenge

--- main.js
+++ main.js
@@ -502,3 +502,41 @@ decodeMorse = function(morseCode){
 function isIsogram(str){
   return new Set(str.toLowerCase()).size == str.length;
 }
+
+//Change seconds into readable format
+function formatDuration (time){
+  if (time == 0)
+    return "now";
+  let array = [];
+  const years = Math.floor(time / 31536000);
+  if (years)
+    array.push(`${years} year${years > 1 ? 's' : ""}`);
+  const days = Math.floor((time % 31536000) / 86400);
+  if (days)
+    array.push(`${days} day${days > 1 ? 's' : ""}`);
+  const hours = Math.floor((time % 86400) / 3600);
+  if (hours)
+    array.push(`${hours} hour${hours > 1 ? 's' : ""}`);
+  const minutes = Math.floor((time % 3600) / 60);
+  if (minutes)
+    array.push(`${minutes} minute${minutes > 1 ? 's' : ""}`);
+  const seconds = time % 60;
+  if (seconds)
+    array.push(`${seconds} second${seconds > 1 ? 's' : ""}`);
+  
+  if (array.length == 1)
+    return array[0];
+  else if (array.length == 2)
+    return array.join(" and ");
+  else{
+    let noComma = array.pop();
+    noComma = array.pop() + " and " + noComma;
+    return array.join(", ") + ", " + noComma;
+  }
+}
+
+//Sort only the odds in ascending order, every even numbers remains in place in the array
+function sortArray(array) {
+  let odds = array.filter(x => x % 2).sort((a, b) => a - b);
+  return array.map(x => (x % 2) ? odds.shift() : x);
+}
