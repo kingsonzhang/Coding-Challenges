8d39d3544b54d192d881b04a83699b9ba2b80c4d
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-04-23 20:47:08 -0700

    April 23, 2022 Coding Challenge

--- main.js
+++ main.js
@@ -664,3 +664,20 @@ function decrypt(encryptedText, n){
     encryptedText = decypher(encryptedText);
   return encryptedText;
 }
+
+//Remove first and last character from string
+function removeChar(str) {
+  return str.slice(1, -1);
+}
+
+//Find two integers in array that add up to given number within time limit
+function sumPairs(ints, s) {
+  let cache = {};
+  for (let i = 0; i < ints.length; i++){
+    if (s - ints[i] in cache)
+      return [s - ints[i], ints[i]]
+    else
+      cache[ints[i]] = i;
+  }
+  return undefined
+}
