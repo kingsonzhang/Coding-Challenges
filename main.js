d579e7df5b43a2efd2a7a83b6918242faecf71f4
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-04-26 01:20:13 -0700

    April 25, 2022 Coding Challenge

--- main.js
+++ main.js
@@ -700,3 +700,9 @@ function stockList(listOfArt, listOfCat){
   })
   return listOfCat.map(x => `(${x} : ${x in dictionary ? dictionary[x] : 0})`).join(" - ")
 }
+
+//Cancel out NORTH/SOUTH and EAST/WEST in a given array
+function dirReduc(arr){
+  const OPPOSITE = {"NORTH":"SOUTH", "SOUTH":"NORTH", "WEST":"EAST", "EAST":"WEST"};
+  return arr.reduce((acc, direction) => {direction == OPPOSITE[acc.slice(-1)] ? acc.pop() : acc.push(direction); return acc}, []);
+}
