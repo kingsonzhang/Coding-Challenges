1b9793c2d8b8974cb7ab3501e50b086c9124ba2d
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-04-27 19:00:49 -0700

    April 27, 2022 Coding Challenge

--- main.js
+++ main.js
@@ -720,3 +720,16 @@ function rowSumOddNumbers(n) {
   return Math.pow(n, 3);
 }
 
+//Max sum of subarray in a array problem
+//Look up Kadane's Algorithm
+//Dynamic programming
+var maxSequence = function(arr){
+  let localMax = 0;
+  let maxSum = 0;
+  for (let i = 0; i < arr.length; i++){
+    localMax = Math.max(arr[i], arr[i] + localMax);
+    if (localMax > maxSum)
+      maxSum = localMax;
+  }
+  return maxSum;
+}
