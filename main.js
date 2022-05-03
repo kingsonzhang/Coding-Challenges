fbf40d3e6e87e3adcef5754c4901cccb666f472d
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-05-03 15:52:57 -0700

    May 3, 2022 Coding Challenge

--- main.js
+++ main.js
@@ -757,3 +757,17 @@ function toCamelCase(str){
   }
   return split.join("");
 }
+
+//Valid parenthesis
+function validParentheses(parens) {
+  let count = 0;
+  for (let i = 0; i < parens.length; i++){
+    if (parens[i] == "(")
+      count++;
+    else 
+      count--;
+    if (count < 0)
+      return false;
+  }
+  return count == 0;
+}
