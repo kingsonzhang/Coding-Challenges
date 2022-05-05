c2d865506f30dbf9b28bdc8386e8971a543a70a3
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-05-04 19:02:48 -0700

    May 4, 2022 Coding Challenge

--- main.js
+++ main.js
@@ -771,3 +771,31 @@ function validParentheses(parens) {
   }
   return count == 0;
 }
+
+//Return squares that make up a rectangle
+function sqInRect(lng, wdth){
+  if (lng == wdth)
+    return null;
+  let total = [];
+  while (lng > 0 && wdth > 0){
+    total.push(Math.min(lng, wdth));
+    lng > wdth ? lng -= wdth : wdth -= lng;
+  }
+  return total;
+}
+
+//Valid IP Numbers
+function isValidIP(str) {
+  const VALUES = str.split(".");
+  console.log(VALUES);
+  //If there are only 4 numbers in the string seperated by periods
+  if (VALUES.length == 4)
+    //If every number only contains numbers (no letters)
+    if (VALUES.every(x => x.split("").every(character => character >= '0' && character <= '9')))
+      //If every number does not have leading 0's
+      if (VALUES.every(y => y == Number(y).toString()))
+        //Return true if the numbers are between 0 and 255 inclusive
+        return VALUES.every(z => Number(z) >= 0 && Number(z) <= 255);
+  //Otherwise by default return false
+  return false;
+}
