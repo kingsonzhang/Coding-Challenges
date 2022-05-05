4141558b3d99dc722caaa1ceab7ea87233804c43
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-05-05 13:52:59 -0700

    May 5, 2022 Coding Challenge

--- main.js
+++ main.js
@@ -799,3 +799,18 @@ function isValidIP(str) {
   //Otherwise by default return false
   return false;
 }
+
+//Sort by last then first name
+function meeting(s) {
+  return s.split(";")
+    .map(x => x.toUpperCase())
+    .map(x => x.split(":"))
+    .sort((a, b) => {
+      if (a[1] > b[1]) return 1;
+      if (a[1] < b[1]) return -1;
+      if (a[0] > b[0]) return 1;
+      if (a[0] < b[0]) return -1;
+    })
+    .map(x => `(${x[1].toUpperCase()}, ${x[0].toUpperCase()})`)
+    .join("");
+}
