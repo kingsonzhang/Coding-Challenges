c408cd38f76c3b2d0b7fa6f140b7c2c5e3f0d832
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-05-06 17:55:00 -0700

    May 6 Coding Challenge

--- main.js
+++ main.js
@@ -814,3 +814,28 @@ function meeting(s) {
     .map(x => `(${x[1].toUpperCase()}, ${x[0].toUpperCase()})`)
     .join("");
 }
+
+//RPN Calculator
+//Very clever use of switching order of pops on stack through number manipulation
+function calc(expr){
+  let numbers = [];
+  expr.split(" ").forEach(x => {
+    switch(x){
+        case '+':
+          numbers.push(numbers.pop() + numbers. pop());
+          break;
+        case '-':
+          numbers.push(-numbers.pop() + numbers.pop());
+          break;
+        case '*':
+          numbers.push(numbers.pop() * numbers.pop());
+          break;
+        case '/':
+          numbers.push(1 / (numbers.pop() / numbers.pop()));
+           break;
+        default:
+          numbers.push(Number(x));
+    }
+  })
+  return numbers.pop();
+}
