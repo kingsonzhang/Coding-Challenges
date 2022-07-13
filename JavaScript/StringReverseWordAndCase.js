d1873706dbb48732e159fdbb517bac61758d9099
kingsonzhang <70171985+kingsonzhang@users.noreply.github.com>
2022-07-12 23:23:22 -0700

    July 12, 2022 Coding Challenge

---
+++ JavaScript/StringReverseWordAndCase.js
@@ -0,0 +1,11 @@
+function stringTransformer(str) {
+  return str
+    .split(' ')
+    .reverse()
+    .join(' ')
+    .split('')
+    .map(v => v == v.toUpperCase() ?
+      v.toLowerCase() :
+      v.toUpperCase())
+    .join('');
+}
