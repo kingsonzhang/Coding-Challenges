0c73d500318104883dc5559ce0697ecc224b88e1
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-10-16 19:16:06 +0000

    October 16, 2022 Coding Challenge

---
+++ JavaScript/LinkedListMergeSorted.js
@@ -0,0 +1,23 @@
+var mergeTwoLists = function(list1, list2) {
+    let head = new ListNode();
+    let cur = head;
+    let cur1 = list1, cur2 = list2;
+    while (cur1 !== null && cur2 !== null) {
+        if (cur1.val < cur2.val) {
+            cur.next = cur1; // point new list to this node
+            cur1 = cur1.next; // advance ptr of list 1
+        } else {
+            cur.next = cur2; // point new list to this node
+            cur2 = cur2.next; // advance ptr of list 2
+        }
+        cur = cur.next; // advance ptr of new list
+    }
+    // in case the end of one list is reached earlier
+    if (cur1 !== null) {
+        cur.next = cur1;
+    }
+    if (cur2 !== null) {
+        cur.next = cur2;
+    }
+    return head.next;
+};
