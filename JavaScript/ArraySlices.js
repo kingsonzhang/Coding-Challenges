84d4bc80350eaccacdb0e59444cdfc66acf67832
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-11-04 18:21:26 +0000

    November 4, 2022 Coding Challenge

---
+++ JavaScript/ArraySlices.js
@@ -0,0 +1,4 @@
+function head(array) { return array[0];}
+function tail(array) {return array.slice(1, array.length);}
+function init(array) { return array.slice(0, array.length-1);}
+function last(array) { return array[array.length-1];}
