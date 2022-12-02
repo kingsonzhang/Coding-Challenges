7dc92ea11e9883233ffbe2df98ea91d42b791491
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-12-02 22:40:02 +0000

    December 2, 2022 Coding Challenge

--- JavaScript/GenerateRandomTime.js
+++ JavaScript/GenerateRandomTime.js
@@ -2,4 +2,4 @@ function getRandomInt(max){
     return Math.floor(Math.random() * max);
 }
 
-console.log(`${getRandomInt(5) + 18}:${getRandomInt(60)}:${getRandomInt(60)}`);
+console.log(`${getRandomInt(5) + 18}:${String(getRandomInt(60)).padStart(2, '0')}:${String(getRandomInt(60)).padStart(2, '0')}`);
